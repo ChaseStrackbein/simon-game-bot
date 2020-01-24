5e1f8fa5608e80a62b78e26136f26ecf190f71b0
Chase Strackbein <chase.strackbein@gmail.com>
2020-01-23 21:00:20 -0500

    Cleanup and comment

--- simon-bot.js
+++ simon-bot.js
@@ -8,19 +8,21 @@ const buttons = [
 ]
 
 // Store state
-let myTurn = false
 let sequence = []
 let currentSequenceSpot = 0
 
+// Attach observer
 const config = { attributes: true, attributeFilter: ['class', 'disabled'], attributeOldValue: true }
 const observer = new MutationObserver(onButtonActive)
 buttons.forEach(btn => observer.observe(btn, config))
 
 function onButtonActive (mutationsList) {
   const mutation = mutationsList[0]
+  // If the button is being marked as active
   if (mutation.attributeName === 'class' &&
   mutation.target.classList.contains('active') &&
   !mutation.oldValue.split(' ').includes('active')) {
+    // If it's the latest button, add to sequence
     if (currentSequenceSpot === sequence.length) {
       sequence.push(mutation.target)
       return
@@ -29,17 +31,19 @@ function onButtonActive (mutationsList) {
     return
   }
   
-  if (mutation.attributeName === 'disabled' && !mutation.target.disabled) {
-    myTurn = true
-    takeTurn()
+  // If the buttons are no longer disabled, it's my turn!
+  if (mutation.attributeName === 'disabled' &&
+    !mutation.target.disabled) {
+    myTurn()
   }
 }
 
-async function takeTurn () {
+async function myTurn () {
   for (let i = 0; i < sequence.length; i++) {
     sequence[i].dispatchEvent(new Event('mousedown'))
     await wait(50)
   }
+  // Reset sequence to prepare to listen
   currentSequenceSpot = 0
 }
 
@@ -47,4 +51,5 @@ async function wait (ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+// Begin play
 playBtn.click()
