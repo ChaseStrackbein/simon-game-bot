cac544e0dc0a3dd83020bb41115a8631735618fe
Chase Strackbein <chase.strackbein@gmail.com>
2020-01-23 20:57:07 -0500

    Finish observer logic, implement bot taking turn

--- simon-bot.js
+++ simon-bot.js
@@ -10,17 +10,41 @@ const buttons = [
 // Store state
 let myTurn = false
 let sequence = []
+let currentSequenceSpot = 0
 
-const config = { attributes: true, attributeFilter: ['class'], attributeOldValue: true }
+const config = { attributes: true, attributeFilter: ['class', 'disabled'], attributeOldValue: true }
 const observer = new MutationObserver(onButtonActive)
 buttons.forEach(btn => observer.observe(btn, config))
 
 function onButtonActive (mutationsList) {
   const mutation = mutationsList[0]
-  debugValue = mutation.oldValue
-  if (mutation.target.classList.contains('active') && !mutation.oldValue.split(' ').includes('active')) {
-    sequence.push(mutation.target)
+  if (mutation.attributeName === 'class' &&
+  mutation.target.classList.contains('active') &&
+  !mutation.oldValue.split(' ').includes('active')) {
+    if (currentSequenceSpot === sequence.length) {
+      sequence.push(mutation.target)
+      return
+    }
+    currentSequenceSpot++
+    return
   }
+  
+  if (mutation.attributeName === 'disabled' && !mutation.target.disabled) {
+    myTurn = true
+    takeTurn()
+  }
+}
+
+async function takeTurn () {
+  for (let i = 0; i < sequence.length; i++) {
+    sequence[i].dispatchEvent(new Event('mousedown'))
+    await wait(50)
+  }
+  currentSequenceSpot = 0
+}
+
+async function wait (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 playBtn.click()
